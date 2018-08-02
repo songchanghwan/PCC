3683c472bacbceb6312d92e2f10c49b83048cdec
PCC Project <songstudy@naver.com>
2018-08-03 00:29:38 +0900

    js 다시 넣음

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -10,6 +10,17 @@ $(".m_menu>span").on('click',function(){
 }).mouseover(function(){
         $(this).next("ul").slideDown();
 });
+
+	/* 히스토리 기능  입니다*/
+	$(document).on('click','#nav a','.hide a',function(e){
+	history.pushState(null, null, e.target.href);
+	$('#section').load(e.target.href + ' #section>.article');
+	event.preventDefault();
+	})
+	$(window).on('popstate', function(e){
+	$('#section').load(location.href + ' #section>.article');
+	})
+
     /* 스크롤탑 기능 */
     var scrollTop = $('#nav a');
     var delay = 500;
