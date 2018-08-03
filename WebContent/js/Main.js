501733903cd2f00a4a91f1554e4d46fd7d400d0e
PCC Project <songstudy@naver.com>
2018-08-04 02:52:14 +0900

    ★ 2018 08 04 최종본 ★

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 /* 슬라이드 메뉴 기능 */
 $(".m_menu>span").on('click',function(){
@@ -10,19 +9,6 @@ $(".m_menu>span").on('click',function(){
     }
 }).mouseover(function(){
         $(this).next("ul").slideDown();
+	})
 });
-=======
-$(document).ready(function() {
-	/* 슬라이드 메뉴 기능 */
-	$(".m_menu>span").on('click', function() {
-		var submenu = $(this).next("ul");
-		if (submenu.is(":visible")) {
-			submenu.slideUp();
-		} else {
-			submenu.slideDown();
-		}
-	}).mouseover(function() {
-		$(this).next("ul").slideDown();
-	});
->>>>>>> 298001279aff748e5b5b3c10a336030c28ef8874
-});
+
